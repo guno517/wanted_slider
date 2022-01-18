2a7e46f7035ddda71763a9fe180ee7576b007ca5
guno517 <60571418+guno517@users.noreply.github.com>
2022-01-18 22:50:18 +0900

    ThemeProvider 삭제

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,13 @@
 import Header from './components/Header';
 import Slider from './components/Slider';
-import { ThemeProvider } from 'styled-components';
-import theme from './components/style/theme'
 
 function App() {
 
   return (
-    <ThemeProvider theme={theme}>
+    <>
       <Header />
       <Slider />
-    </ThemeProvider>
+    </>
   );
 }
 
